c384499e0d6694875c8822dc5c69613976d10cf0
LuizHNR <luizhneri12@gmail.com>
2024-08-14 10:48:48 -0300

    teste

--- aula-04-2sem/aula04/src/components/Main.tsx
+++ aula-04-2sem/aula04/src/components/Main.tsx
@@ -5,6 +5,13 @@ export default function Main(){
         <div>
             <main>
                 <h1>Main</h1>
+                <nav>
+                    <ul>
+                        <li><a href="https://github.com/LuizHNR" target="_blank">GitHub</a></li>
+                        <li><a href="https://www2.fiap.com.br/Aluno/Home/#aulas" target="_blank">Portal Fiap</a></li>
+                        <li><a href="https://teams.microsoft.com/v2/" target="_blank">Teams</a></li>
+                    </ul>
+                </nav>
                 <Conteudo/>
             </main>
         </div>
